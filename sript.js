7f7ab6dd078a120ded2f6ccfee5d9a257dc1b7f1
Adamlegen <adammukhammed@gmail.com>
2024-02-24 21:26:38 +0500

    java

---
+++ sript.js
@@ -0,0 +1,9 @@
+
+
+let a=prompt("string typda malumot kiriting")
+let b=+prompt("number typda malumot kiriting")
+
+alert(`
+siz kiritgan ${a} sozi ${typeof (a)} turiga kiradi
+siz kiritgan ${b} raqami ${typeof (b)} turiga kiradi
+`)
