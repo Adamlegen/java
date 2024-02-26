ccd2ff18d368a1ab66092afb100cb97607474e42
Adamlegen <adammukhammed@gmail.com>
2024-02-26 18:43:48 +0500

    js2

--- sript.js
+++ sript.js
@@ -2,8 +2,12 @@
 
 let a=prompt("string typda malumot kiriting")
 let b=+prompt("number typda malumot kiriting")
+let c=+prompt("number typda malumot kiriting")
 
 alert(`
 siz kiritgan ${a} sozi ${typeof (a)} turiga kiradi
 siz kiritgan ${b} raqami ${typeof (b)} turiga kiradi
+siz kiritgan ${c} raqami ${typeof (c)} turiga kiradi
+
 `)
+
